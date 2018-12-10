93fd986cba80df882a2834d3c9979b5756d0063d
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-10 03:38:05 -0600

    Evaluacion: setters and getters done.

--- Models/Evaluacion/Evaluacion.cpp
+++ Models/Evaluacion/Evaluacion.cpp
@@ -3,3 +3,70 @@
 //
 
 #include "Evaluacion.h"
+#include "../Alumno/Alumno.h"
+#include "../Asignatura/Asignatura.h"
+
+namespace Models
+{
+    Evaluacion::Evaluacion(std::string Name) : AcademyBase()
+    {
+        this->setName(Name);
+    }
+
+    const int & Evaluacion::getId() const
+    {
+        return AcademyBase::getId();
+    }
+
+    std::string const & Evaluacion::getName() const
+    {
+        return AcademyBase::getName();
+    }
+
+    float const & Evaluacion::getNota() const {
+        return Nota;
+    }
+
+    int const & Evaluacion::getAlumnoId() const {
+        return AlumnoId;
+    }
+
+    const std::shared_ptr<Alumno> &Evaluacion::getAlumnoOwner() const {
+        return AlumnoOwner;
+    }
+
+    int Evaluacion::getAsignaturaId() const {
+        return AsignaturaId;
+    }
+
+    const std::shared_ptr<Asignatura> &Evaluacion::getAsignaturaOwner() const {
+        return AsignaturaOwner;
+    }
+
+    void Evaluacion::setName(std::string Name)
+    {
+        AcademyBase::setName(Name);
+    }
+
+    void Evaluacion::setNota(float Nota) {
+        this->Nota = Nota;
+    }
+
+    void Evaluacion::setAlumnoId(int AlumnoId) {
+        this->AlumnoId = AlumnoId;
+    }
+
+    void Evaluacion::setAlumnoOwner(const std::shared_ptr<Alumno> &AlumnoOwner) {
+        this->AlumnoOwner = nullptr;
+        this->AlumnoOwner = AlumnoOwner;
+    }
+
+    void Evaluacion::setAsignaturaId(int AsignaturaId) {
+        this->AsignaturaId = AsignaturaId;
+    }
+
+    void Evaluacion::setAsignaturaOwner(const std::shared_ptr<Asignatura> &AsignaturaOwner) {
+        this->AsignaturaOwner = nullptr;
+        this->AsignaturaOwner = AsignaturaOwner;
+    }
+}
--- Models/Evaluacion/Evaluacion.h
+++ Models/Evaluacion/Evaluacion.h
@@ -4,16 +4,41 @@
 
 #pragma once
 
+#include <memory>
 #include "../AcademyBase/AcademyBase.h"
 
 namespace Models
 {
+    class Alumno;
+    class Asignatura;
+
     class Evaluacion : AcademyBase
     {
+        private:
+            float Nota;
+
+            //owners:
+            int AlumnoId;
+            std::shared_ptr<Alumno> AlumnoOwner;
+            int AsignaturaId;
+            std::shared_ptr<Asignatura> AsignaturaOwner;
+
         public:
-            Evaluacion(std::string Name) : AcademyBase()
-            {
-                this->setName(Name);
-            }
+            Evaluacion(std::string Name); //constructor
+
+            int const & getId() const;
+            std::string const & getName() const;
+            float const & getNota() const;
+            int const & getAlumnoId() const;
+            const std::shared_ptr<Alumno> &getAlumnoOwner() const;
+            int getAsignaturaId() const;
+            const std::shared_ptr<Asignatura> &getAsignaturaOwner() const;
+
+            void setName(std::string);
+            void setNota(float Nota);
+            void setAlumnoId(int AlumnoId);
+            void setAlumnoOwner(const std::shared_ptr<Alumno> &AlumnoOwner);
+            void setAsignaturaId(int AsignaturaId);
+            void setAsignaturaOwner(const std::shared_ptr<Asignatura> &AsignaturaOwner);
     };
 }
