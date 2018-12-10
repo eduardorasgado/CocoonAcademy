e63de56ae90077dd8d230e652dcf544c1275605f
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-10 02:39:07 -0600

    Alumno: Tester-> vector of random quantity of alumnos are generated using different functions.

--- Testers/Models/AlumnoTester.h
+++ Testers/Models/AlumnoTester.h
@@ -3,6 +3,10 @@
 //
 #pragma once
 
+#include <ctime> // std::time
+#include <algorithm> // std::random_shuffle
+#include <cstdlib> // std::rand, std::srand
+
 using namespace Models;
 
 namespace Testers
@@ -12,9 +16,20 @@ namespace Testers
         public:
             static bool testingAlumnoModelTester()
             {
+                // a seed for random generator
+                std::srand (unsigned (std::time(0)));
+
                 std::cout << "[ALUMNO MODEL TESTING]" << std::endl;
                 try {
                     auto alumnos = createAlumnoVector();
+                    // shuffle all objects
+                    randomAlumnos(alumnos);
+
+                    for(auto& alumno : alumnos)
+                    {
+                        std::cout << alumno->getName() << std::endl;
+                    }
+                    std::cout << ">Alumnos creados: " << alumnos.size() << std::endl;
 
                 } catch (std::exception& e)
                 {
@@ -28,15 +43,21 @@ namespace Testers
             static std::vector<std::shared_ptr<Alumno>> createAlumnoVector()
             {
                 auto alumnosList = std::vector<std::shared_ptr<Alumno>>();
-                auto alumnosNames = createNames();
+
+                auto randomAlumnosQuantity = randomNumber(20, 25);
+
+                auto alumnosNames = createNames(randomAlumnosQuantity);
 
                 for (auto& alumno : alumnosNames) {
-                    std::cout << "alumno: " << alumno << std::endl;
+                    // creating alumno with name and introducing into vector
+                    auto newAlumno = std::make_shared<Alumno>(alumno);
+                    alumnosList.push_back(newAlumno);
                 }
+
                 return alumnosList;
             }
 
-            static std::vector<std::string> createNames()
+            static std::vector<std::string> createNames(int& quantity)
             {
                 std::string names[] = {"Eduardo", "Lorenzo", "Martin", "Joaquin", "Juan"};
                 std::string lastNames[] = {"Martinez", "Rasgado", "Sanchez", "Aquino", "Smith"};
@@ -52,7 +73,32 @@ namespace Testers
                     }
                 }
 
-                return listNames;
+                auto vectorNames = randomNames(listNames, quantity);
+
+                return vectorNames;
+            }
+
+            static std::vector<std::string> randomNames(std::vector<std::string>& listNames, int& quantity)
+            {
+                // taking just a range of a vector, from 0 to quantity
+                std::vector<std::string>::const_iterator first = listNames.begin();
+                std::vector<std::string>::const_iterator last = listNames.begin()+quantity;
+                std::vector<std::string> vectorNames(first, last);
+
+                return vectorNames;
+            }
+
+            // a random generator
+            static int randomNumber(int begin, int end)
+            {
+                return begin + (std::rand()%(end-begin));
+            }
+
+
+
+            static void randomAlumnos(std::vector<std::shared_ptr<Alumno>>& alumnos)
+            {
+                std::random_shuffle(alumnos.begin(), alumnos.end());
             }
 
         private:
