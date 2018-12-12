08c34cffdf25374f83515ba5da869eff1d6ba718
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-11 23:48:04 -0600

    Evaluacion: Tester: evaluaciones vector and including names and Notas.

--- Testers/Models/EvaluacionTester.h
+++ Testers/Models/EvaluacionTester.h
@@ -15,7 +15,11 @@ namespace Testers
                 try
                 {
                     // TODO: Testing Logic
-                    auto evaluaciones = std::vector<std::shared_ptr<Evaluacion>>();
+                    auto evaluaciones = createEvalVector();
+                    for(auto& eval : evaluaciones)
+                    {
+                        std::cout << eval->getName() << ", Nota: " << eval->getNota() << std::endl;
+                    }
 
                 } catch(std::exception& e)
                 {
@@ -24,6 +28,25 @@ namespace Testers
                 }
                 std::cout << "[o] Evaluacion Model Test Passed." << std::endl;
                 return true;
+            }
+
+            static std::vector<std::shared_ptr<Evaluacion>> createEvalVector()
+            {
+                // creating a vector of evals with names
+                auto evaluaciones = std::vector<std::shared_ptr<Evaluacion>>();
+                for (int i = 0; i < 30; ++i) {
+                    auto s = std::to_string(i);
+                    auto unidad = "Unidad " + s;
+                    auto eval = std::make_shared<Evaluacion>(unidad);
+                    addNota(eval);
+                    evaluaciones.push_back(eval);
+                }
+                return evaluaciones;
+            }
+
+            static void addNota(std::shared_ptr<Evaluacion> eval)
+            {
+                eval->setNota(9.2);
             }
 
         private:
